38baca5f7b976915e35a8bcd130770726d2c18b7
synthy1 <jackhopson69@gmail.com>
2023-03-19 01:00:08 +0000

    textures

--- Assets/Scripts/cam.cs
+++ Assets/Scripts/cam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cam : MonoBehaviour
+public class Cam : MonoBehaviour
 {
     [Header("Refrences")]
     CharacterController playerBools;
